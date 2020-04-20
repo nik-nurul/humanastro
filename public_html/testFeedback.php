ea939240776aa4578b43e76f113996662bb7dbbf
Danvers Flett <danversflett@gmail.com>
2020-04-20 11:09:51 +1000

    Testing form submission with PHP to text file

---
+++ public_html/testFeedback.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!-- test from https://www.codespeedy.com/save-html-form-data-in-a-txt-text-file-in-php/ -->
+<html>
+<head>
+  <title>Store form data in .txt file</title>
+</head>
+<body>
+  <form method="post">
+    Enter Your Text Here:<br>
+    <input type="text" name="textdata"><br>
+    <input type="submit" name="submit">
+    
+  </form>
+</body>
+</html>
+<?php
+              
+if(isset($_POST['textdata']))
+{
+$data=$_POST['textdata'];
+$fp = fopen('data.txt', 'a');
+fwrite($fp, $data);
+fclose($fp);
+}
+?>

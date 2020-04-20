e85f54bbd8d24d1931297ce9ba5982b3fcc20551
Danvers Flett <danversflett@gmail.com>
2020-04-20 21:19:28 +1000

    Trying file_put_contents() on the advice of Fafa

--- public_html/testFeedback.php
+++ public_html/testFeedback.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <!-- test from https://www.codespeedy.com/save-html-form-data-in-a-txt-text-file-in-php/ -->
-<!-- new master -->
+<!-- first example now using file_put_contents() -- Fafa's idea -->
 <html>
 <head>
   <title>Store form data in .txt file</title>
@@ -8,9 +8,9 @@
 <body>
   <form method="post">
     Enter Your Text Here:<br>
-    <input type="text" name="textdata"><br>
+    <textarea name="comment" rows="10" cols="60"></textarea>
+	<br>
     <input type="submit" name="submit">
-    
   </form>
 </body>
 </html>
@@ -18,9 +18,10 @@
               
 if(isset($_POST['textdata']))
 {
-$data=$_POST['textdata'];
-$fp = fopen('data.txt', 'a');
-fwrite($fp, $data);
-fclose($fp);
+	$data=$_POST['textdata'];
+	file_put_contents('data.txt', $data, FILE_APPEND);
+//	$fp = fopen('data.txt', 'a');
+//	fwrite($fp, $data);
+//	fclose($fp);
 }
 ?>

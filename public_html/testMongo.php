a8e331b2ee907566aa8b63bbfea37839b5ef3cbd
Danvers Flett <danversflett@gmail.com>
2020-04-22 10:06:57 +1000

    Mongo Test

--- public_html/testMongo.php
+++ public_html/testMongo.php
@@ -2,6 +2,10 @@
 <body>
 MongoDB Test
 <?php
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
 	$mongo = new MongoDB\Client('mongodb://127.0.0.1:27017');
 	$dbs = $mongo->listDatabases();
 ?>

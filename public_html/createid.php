43cf7843d0a5902666712a2f811cda1caad0b4a7
humanastro.csproject.org <swe40001.grp3.2020@gmail.com>
2020-05-13 04:28:26 +0000

    Added consent and age handling, session variables for consent and user ID - TO DO - resume suspended sessions

--- public_html/createid.php
+++ public_html/createid.php
@@ -1,9 +1,10 @@
-<!DOCTYPE html>
 <?php
+session_start(); // used to pass userId from page to page
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 ?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="utf-8" />
@@ -36,126 +37,185 @@ error_reporting(E_ALL);
 		<form id="demoform" action="experience.php" method="post"> 
 		<!--The action should be change to the URL where we want to save the data from the form-->
 <?php
+
+/* TO DO
+	use GET user ID to resume sessions - but!
+		do not allow user to see what was previously entered
+		i.e. do not populate questions with previous responses
+			(this would be a privacy breach)
+			if user resumes a demographic page, they have to re-enter responses
+	
+	maybe create a resume.php page - enter the user ID as a GET variable
+		the user record keeps a track of the page the user is up to
+		- redirect to that page
+
+	attempting to resume directly via other pages will cause user to jump to home page
+
+	set conditions for creation of new User ID or re-use existing ID:
+		from consent.php -
+			if consent given = yes
+				pass "consent" =true POST var to createid.php
+		
+		on createid.php
+		if
+			SESSION user ID is blank
+			and
+			consent=yes
+				create new userid
+				store questions
+				store consent
+				serve demographic questions
+		
+*/
+
 	$dbName = 'humanastro';		// database name
 	$Dcoll = 'demographic_Qs';	// question collection name
 	$Ecoll = 'experience_Qs';	// experience collection name
 	$Ucoll = 'users';			// user collection name
 
 try {
-	$manager = new MongoDB\Driver\Manager("mongodb://localhost:27017"); // connect to the Mongo DB
+	// Consent - "Yes" button was clicked - flag consent as true in session 
+	// the "consent" propery in the user document in the database
+	// is the canonical record of consent
+	// - this must override other variables
+	if ( isset($_POST["consent"]) and $_POST["consent"] == "true"){
 
-	// create new user record
+		$_SESSION["consent"] = true;
+		$manager = new MongoDB\Driver\Manager("mongodb://localhost:27017"); // connect to the Mongo DB
 
-	$newUser = [
-		"demographic_data" => [],
-		"experience_data" => [],
-		"task_data" => []
-	];
-	
-	$bulk = new MongoDB\Driver\BulkWrite(['ordered' => true]);
-	$userId = $bulk->insert($newUser);
-	echo '
-			<input type="hidden" name="userId" value="'.$userId.'"/>';	
-	
-	// create user ID object with which to ID the user record
-	$_id = new MongoDB\BSON\ObjectID($userId);
-	$filter = [ "_id" => $_id ]; // return only this user
+		// create new user record
+		$newUser = [
+			"consent" => true,
+			"demographic_data" => [],
+			"experience_data" => [],
+			"task_data" => []
+		];
 
-// read all documents in $Dcoll and $Ecoll
-    $query = new MongoDB\Driver\Query([]); // [] means get all documents
-
-	// write each demographic question document into the user document
-    $rows = $manager->executeQuery($dbName.'.'.$Dcoll, $query);    	
-    foreach ($rows as $row) {
-		$bulk->update(
-			$filter,
-			[ '$push' => [ "demographic_data" => $row ]]
-		);
-    }
-
-	// write each experience question document into the user document
-     $rows = $manager->executeQuery($dbName.'.'.$Ecoll, $query);    	
-    foreach ($rows as $row) {
-		$bulk->update(
-			$filter,
-			[ '$push' => [ "experience_data" => $row ]]
-		);
-    }
-	
-	$result = $manager->executeBulkWrite($dbName.'.'.$Ucoll, $bulk);
-
-////////////
-	
-	// set up a query to retrieve questions from the new user record in the database
-	$options = ['sort' => ['question_num' => 1]]; // sort the results based on question_num
-	$query = new MongoDB\Driver\Query($filter,$options); // create a query object with the above parameters
-	
-    $cursor = $manager->executeQuery($dbName.'.'.$Ucoll, $query); // execute and return a cursor
-	// the cursor allows us to iterate over the query results
-	
-// retrieve user	
-	$iterator = new IteratorIterator($cursor);
-	$iterator->rewind();
-	$userDoc = $iterator->current();
-	$demographic_Qs = $userDoc->demographic_data;
-	
-//	echo '\n<pre>\n';
-//	var_dump($demographic_Qs);
-//	echo '\n</pre>\n';
-	
-	foreach ($demographic_Qs as $q){
-		$question = $q->question; // the full text of the question
-		$q_id = $q->q_id;		// the abbreviated question identifier
+		$bulk = new MongoDB\Driver\BulkWrite(['ordered' => true]);
+		$userId = $bulk->insert($newUser);
+		
+		// pass the user ID on to the next page
+		// via a session variable
+		$_SESSION["userId"] = (string)$userId;
+		// also via the HTML form
 		echo '
-				<!--'.$question.'-->';
-	
-		// handle the different question types - radio or dropdown
-		switch ($q->q_type){
-			
-			case "radio":
-				echo '
-				<p>'.$question.':
-				<br/>';
-				foreach ($q->options as $optObj){
-					$o = $optObj->option;	// the full text of the question option
-					$o_id = $optObj->opt_id; // the abbreviated option identifier
-					echo'
-					<input type="radio" id="'.$o_id.'" name="'.$q_id.'" value="'.$o_id.'" required="required"/>
-						<label for="'.$o_id.'">'.$o.'</label>
+				<input type="hidden" name="userId" value="'.(string)$userId.'"/>';	
+		
+		// create user ID object with which to ID the user record
+		$_id = new MongoDB\BSON\ObjectID($userId);
+		$filter = [ "_id" => $_id ]; // return only this user
+
+	// debug
+		echo '<p>DEBUG -- $_POST: <br><pre>';
+		var_dump($_POST);
+		echo '</pre>';
+		echo '<p>DEBUG -- $_SESSION: <br><pre>';
+		var_dump($_SESSION);
+		echo '</pre>';
+
+
+	// read all documents in $Dcoll and $Ecoll
+		$query = new MongoDB\Driver\Query([]); // [] means get all documents
+
+		// write each demographic question document into the user document
+		$rows = $manager->executeQuery($dbName.'.'.$Dcoll, $query);    	
+		foreach ($rows as $row) {
+			$bulk->update(
+				$filter,
+				[ '$push' => [ "demographic_data" => $row ]]
+			);
+		}
+
+		// write each experience question document into the user document
+		 $rows = $manager->executeQuery($dbName.'.'.$Ecoll, $query);    	
+		foreach ($rows as $row) {
+			$bulk->update(
+				$filter,
+				[ '$push' => [ "experience_data" => $row ]]
+			);
+		}
+		
+		$result = $manager->executeBulkWrite($dbName.'.'.$Ucoll, $bulk);
+
+	////////////
+		
+		// set up a query to retrieve questions from the new user record in the database
+		$options = ['sort' => ['question_num' => 1]]; // sort the results based on question_num
+		$query = new MongoDB\Driver\Query($filter,$options); // create a query object with the above parameters
+		
+		$cursor = $manager->executeQuery($dbName.'.'.$Ucoll, $query); // execute and return a cursor
+		// the cursor allows us to iterate over the query results
+		
+	// retrieve user	
+		// there should only be one result - this makes sure we get the only result
+		$iterator = new IteratorIterator($cursor); // generate an iterator from the cursor
+		$iterator->rewind(); // make sure the iterator is at the first element (head)
+		$userDoc = $iterator->current(); // retrieve the user document from the iterator
+		$demographic_Qs = $userDoc->demographic_data;
+
+		foreach ($demographic_Qs as $q){
+			$question = $q->question; // the full text of the question
+			$q_id = $q->q_id;		// the abbreviated question identifier
+			echo '
+					<!--'.$question.'-->';
+		
+			// handle the different question types - radio or dropdown
+			switch ($q->q_type){
+				
+				case "radio":
+					echo '
+					<p>'.$question.':
 					<br/>';
-					// handle questions that allow a freetext answer
-					if ( isset($optObj->freetext_answer) && $optObj->freetext_answer ){
+					foreach ($q->options as $optObj){
+						$o = $optObj->option;	// the full text of the question option
+						$o_id = $optObj->opt_id; // the abbreviated option identifier
 						echo'
-						<label for="'.$optObj->freetext_id.'">'.$optObj->freetext_desc.'</label> 
-							<input class="resize" type="text" name= "'.$optObj->freetext_id.'" id="'.$optObj->freetext_id.'" maxlength="'.$optObj->freetext_length.'" size="'.$optObj->freetext_length.'"/>';
+						<input type="radio" id="'.$o_id.'" name="'.$q_id.'" value="'.$o_id.'" required="required"/>
+							<label for="'.$o_id.'">'.$o.'</label>
+						<br/>';
+						// handle questions that allow a freetext answer
+						if ( isset($optObj->freetext_answer) && $optObj->freetext_answer ){
+							echo'
+							<label for="'.$optObj->freetext_id.'">'.$optObj->freetext_desc.'</label> 
+								<input class="resize" type="text" name= "'.$optObj->freetext_id.'" id="'.$optObj->freetext_id.'" maxlength="'.$optObj->freetext_length.'" size="'.$optObj->freetext_length.'"/>';
+						}
 					}
-				}
-				echo '
-				<p>
+					echo '
+					<p>
 ';
-				break;
-				
-			case "dropdown":
-				echo '
-				<p><label for="'.$q_id.'">'.$question.'</label>
-				<br/>				
-					<select class="selectsize" name="'.$q_id.'" id="'.$q_id.'" required="required">
-						<option value="">Please Select</option>';
-				foreach ($q->options as $optObj){
-					$o = $optObj->option;	// the full text of the question option
-					$o_id = $optObj->opt_id; // the abbreviated option identifier
-					echo'
-						<option id="'.$o_id.'" value="'.$o_id.'">'.$o.'</option>';
-				}
-				echo '
-					</select>
-				<p>
+					break;
+					
+				case "dropdown":
+					echo '
+					<p><label for="'.$q_id.'">'.$question.'</label>
+					<br/>				
+						<select class="selectsize" name="'.$q_id.'" id="'.$q_id.'" required="required">
+							<option value="">Please Select</option>';
+					foreach ($q->options as $optObj){
+						$o = $optObj->option;	// the full text of the question option
+						$o_id = $optObj->opt_id; // the abbreviated option identifier
+						echo'
+							<option id="'.$o_id.'" value="'.$o_id.'">'.$o.'</option>';
+					}
+					echo '
+						</select>
+					<p>
 ';
-				break;
-				
-			default:
-				echo '\n				<p>Error! Question Type not specified\n';
-		}		
+					break;
+					
+				default:
+					echo '\n				<p>Error! Question Type not specified\n';
+			}		
+		}
+		
+ // end if consent == true	
+	} else {
+		$_SESSION["consent"] = false;
+
+		echo '
+		<p>Consent was not given</p>
+		<p>Do something!</p>
+';
 	}
 
 // exception handling for the database connection	
--- public_html/experience.php
+++ public_html/experience.php
@@ -1,9 +1,10 @@
-<!DOCTYPE html>
 <?php
+session_start(); // used to pass userId from page to page
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 ?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
   <title>Experience Question</title>
@@ -41,107 +42,180 @@ error_reporting(E_ALL);
 	<div class="sliders">	 
 	  <!-- qustion section. Used radio button-->
 	  <p> Please select your answer </p><br/>
-	  <form>
+		<!--Form starts here-->
+		<form id="expform" action="calibration.php" method="post"> 
+		<!--The action should be change to the URL where we want to save the data from the form-->
 	  
 <?php
-	echo '<p>$_POST: <br><pre>';
+// debug
+	echo '<p>DEBUG -- $_POST: <br><pre>';
 	var_dump($_POST);
 	echo '</pre>';
+	echo '<p>DEBUG -- $_SESSION: <br><pre>';
+	var_dump($_SESSION);
+	echo '</pre>';
 
-	$dbName = 'humanastro';		// database name
-	$Qcoll = 'experience_Qs';	// experience collection name
-	$Ucoll = 'users';			// user collection name
+	// don't do anything if consent is not true
+	if ( isset( $_SESSION["consent"] ) and $_SESSION["consent"] ) {
 
-	$userId = $_POST["userId"]; // extract userID string
-	unset($_POST["userId"]); // remove userID from POST array
-	  
-try {
-	$manager = new MongoDB\Driver\Manager("mongodb://localhost:27017"); // connect to the Mongo DB
+		// update user record with demographic answers
 
-	// add the demographic answers to the user record
-	$bulk = new MongoDB\Driver\BulkWrite(['ordered' => true]);
+		$dbName = 'humanastro';		// database name
+		$Ucoll = 'users';			// user collection name
 
-	// create user ID object with which to ID the user record
-	$_id = new MongoDB\BSON\ObjectID($userId);
+		$userId = $_SESSION["userId"];
+	//	$userId = $_POST["userId"]; // extract userID string
+		unset($_POST["userId"]); // remove userID from POST array
+
+		// pass the user ID on to the next page
+		echo '
+				<input type="hidden" name="userId" value="'.$userId.'"/>';	
+
+		  
+		try {
+			$manager = new MongoDB\Driver\Manager("mongodb://localhost:27017"); // connect to the Mongo DB
+
+			// **** add the demographic answers to the user record ****
+			$bulk = new MongoDB\Driver\BulkWrite(['ordered' => true]);
+
+			// create user ID object with which to ID the user record
+			$_id = new MongoDB\BSON\ObjectID($userId);
 
 
-// mongo shell - working example of how to set the "answer" property inside the question element of the "demographic_data" array
-// db.users.updateOne({"_id": ObjectId("5eb8b54323e858761f1ca452"), "demographic_data": {$elemMatch: {"q_id":"astarea"}}},{$set: {"demographic_data.$.answer":"foo"}})
+			// mongo shell - working example of how to set the "answer" property inside the question element of the "demographic_data" array
+			// db.users.updateOne({"_id": ObjectId("5eb8b54323e858761f1ca452"), "demographic_data": {$elemMatch: {"q_id":"astarea"}}},{$set: {"demographic_data.$.answer":"foo"}})
 
-	// iterate through each demographic answer, add answer to question element in user record
-    foreach ($_POST as $q_id => $value) {
-		// find specific question in this user document
-		$filter = [
-			"_id" => $_id, // this is the document or object (user, in this case) ID
-			"demographic_data" => [ '$elemMatch' => [ "q_id" => $q_id ] ] // demographic_data contains questions and answers
-		];
-		// set the answer value for this question
-		$bulk->update(
-			$filter,
-			[ '$set' => [ "demographic_data.$.answer" => $value ]]
-		);
-    }	
-	// execute writing all the demographic answers
-	$result = $manager->executeBulkWrite($dbName.'.'.$Ucoll, $bulk);
-	
-	// set up a query to retrieve questions from the database
-	$filter = []; // return all documents (filter is empty)
-	$options = ['sort' => ['question_num' => 1]]; // sort the results based on question_num; 1 means ascending and -1 means descending
-	$query = new MongoDB\Driver\Query($filter,$options); // create a query object with the above parameters
-	
-	// the cursor allows us to iterate over the query results
-	$cursor = $manager->executeQuery($dbName.'.'.$Qcoll, $query); // execute and return a cursor
-	
-	// iterate over each question document
-	foreach ($cursor as $q){
-		$question = $q->question; // the full text of the question
-		$q_id = $q->q_id;		// the abbreviated question identifier
-		$q_min = $q->q_min;		// mininum value allowed
-		$q_max = $q->q_max;		// maximum value allowed
-		$q_steps = $q->q_steps;	// steps in likert scale
-		$q_value = $q->q_value;	// specifies default value
-		
-		echo '<p>'.$question.'  
-			<div class="range">
-				<input type="range" min="'.$q_min.'" max="'.$q_max.'" steps="'.$q_steps.'" value="'.$q_value.'">
-			</div>
-				<ul class="range-labels">';
+			// OVERVIEW - Algorithm:
+			// * iterate through each demographic question in user record
+			// * get the question ID
+			// * match that to the question ID in the $_POST array
+			// * add the answer from the $_POST array to the demographic question in the user record
+			// -- this method is used instead of iterating over the $_POST array as that is less secure
+			// -- iterating over submitted data risks writing corrupted or malicious user input to the DB
 
-		foreach ($q->options as $optObj){
-			$o = $optObj->option;	// the full text of the question option
-			echo '<li>'.$o.'</li>';
+			// set up a query to retrieve questions from the new user record in the database
+			$q_filter = [ "_id" => $_id ]; // just get the user whole user record
+			$q_options = ['sort' => ['question_num' => 1]]; // sort the results based on question_num
+			$query = new MongoDB\Driver\Query( $q_filter, $q_options ); // create a query object with the above parameters
+			$cursor = $manager->executeQuery( $dbName.'.'.$Ucoll, $query ); // execute and return a cursor
+			
+			// retrieve user	
+			// there should only be one result - this makes sure we get the only result
+			$iterator = new IteratorIterator($cursor); // generate an iterator from the cursor
+			$iterator->rewind(); // make sure the iterator is at the first element (head)
+			$userDoc = $iterator->current(); // retrieve the user document from the iterator
+
+			// special handling for the "age" question
+			// - under 18s have consent to record data revoked
+			$isAdult = false;
+
+			// is the user an adult?
+			if ( isset($_POST["Age"] ) and (
+					(in_array($_POST["Age"], array("18-25", "26-35", "36-45", ">45") ) )
+				)
+			) {
+				// iterate through the demographic questions to set their answers
+				foreach ($userDoc->demographic_data as $q){
+					$q_id = $q->q_id;		// the abbreviated question identifier
+					
+					// set the answer, if it exists in the $_POST array
+					if (isset($_POST[$q_id])) {
+						$answer = $_POST[$q_id];
+						// find specific question in this user document
+						$u_filter = [
+							"_id" => $_id, // this is the document or object (user, in this case) ID
+							"demographic_data" => [ '$elemMatch' => [ "q_id" => $q_id ] ] // demographic_data contains questions and answers
+						];
+						// set the answer value for this question
+						$bulk->update(
+							$u_filter,
+							[ '$set' => [ "demographic_data.$.answer" => $answer ]]
+						);
+					}
+				}
+
+				// execute writing all the demographic answers (if the user is an adult)
+				$result = $manager->executeBulkWrite($dbName.'.'.$Ucoll, $bulk);
+
+				// ****** output experience questions ***********
+				// we already have the user document from the above -- $userDoc
+				// which contains the experience questions
+				
+				// iterate over each experience question
+				foreach ($userDoc->experience_data as $q){
+					$question = $q->question; // the full text of the question
+					$q_id = $q->q_id;		// the abbreviated question identifier
+					$q_min = $q->q_min;		// mininum value allowed
+					$q_max = $q->q_max;		// maximum value allowed
+					$q_steps = $q->q_steps;	// steps in likert scale
+					$q_value = $q->q_value;	// specifies default value
+					
+					echo '<p>'.$question.'  
+						<div class="range">
+							<input type="range" name="'.$q_id.'" id="'.$q_id.'" min="'.$q_min.'" max="'.$q_max.'" steps="'.$q_steps.'" value="'.$q_value.'">
+						</div>
+							<ul class="range-labels">';
+
+					foreach ($q->options as $optObj){
+						$o = $optObj->option;	// the full text of the question option
+						echo '<li>'.$o.'</li>';
+					}
+					echo '</ul>
+					</p>
+					<br/>
+					<br/>';
+				}
+				echo '
+				</div>
+				</br>
+				</br>';
+			
+			} else {
+				$_SESSION["consent"] = false;
+				// set the user consent to false in the DB
+				$bulk->update(
+					[ "_id" => $_id ],
+					[ '$set' => [ "consent" => false ]]
+				);
+				$result = $manager->executeBulkWrite($dbName.'.'.$Ucoll, $bulk);
+				
+				echo '
+				<p>Your cannot participate in this research if you are not an adult</p>
+				<p>Your consent to have your details recorded has been withdrawn</p>
+				<p>Do something!</p>
+';
+			}
+			
+		// exception handling for the database connection	
+		} catch (MongoDB\Driver\Exception\Exception $e) {
+
+			$filename = basename(__FILE__);
+			
+			echo "The $filename script has experienced an error.\n"; 
+			echo "It failed with the following exception:\n";
+			
+			echo "Exception:", $e->getMessage(), "\n";
+			echo "In file:", $e->getFile(), "\n";
+			echo "On line:", $e->getLine(), "\n";       
 		}
-		echo '</ul>
-		</p>
-		<br/>
-		<br/>';
-	}
-	echo '</form>
-	</div>
-	</br>
-	</br>';
-// exception handling for the database connection	
-} catch (MongoDB\Driver\Exception\Exception $e) {
-
-    $filename = basename(__FILE__);
-    
-    echo "The $filename script has experienced an error.\n"; 
-    echo "It failed with the following exception:\n";
-    
-    echo "Exception:", $e->getMessage(), "\n";
-    echo "In file:", $e->getFile(), "\n";
-    echo "On line:", $e->getLine(), "\n";       
-}
+	// end of if consent is true
+	} else {
+		// consent was false
+		echo '
+		<p>Consent was not given</p>
+		<p>Do something!</p>
+';
+	}		
 ?>
-	  <!-- section for buttons  -->
-	  <div>
-		<!-- this button will redirect to homepage -->
-		<a href="home.html"><input id="quitBttn" type= "reset" value="Exit to Home"/></a>
-		<!-- this button will redirect to webcam calibration page-->
-		<a href="calibration.html"><input id="submitBttn" type= "submit" value="Submit and continue"/></a>
-	  </div>
-	  <br/>
-	
+		  <!-- section for buttons  -->
+		  <div>
+			<!-- this button will redirect to homepage -->
+			<a href="home.html"><input id="quitBttn" type= "reset" value="Exit to Home"/></a>
+			<!-- this button will redirect to webcam calibration page-->
+			<input id="inpbutton" type="submit" value="Submit and Continue"/>
+		  </div>
+		  <br/>
+		</form>
 
 </body>
 </html>

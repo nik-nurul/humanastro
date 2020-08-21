67fe6c874a12c68ed2f0cfebf2c929cc40d7fbb9
humanastro.csproject.org <swe40001.grp3.2020@gmail.com>
2020-08-21 07:10:12 +0000

    Modified Michael Choi's code to demonstrate sending GazeData to MongoDB via AJAX

--- public_html/gazecloud/index.php
+++ public_html/gazecloud/index.php
@@ -3,6 +3,11 @@
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
+
+	// user ID - this would be the user ID already created in the demographic question process
+	$_id = new MongoDB\BSON\ObjectId();
+	$userIdStr = (string)$_id;
+
 ?>
 
 <!DOCTYPE HTML >
@@ -15,54 +20,66 @@
 			overflow: hidden;
         }
 		</style>
-		<script type = "text/javascript" >
-		//Init arrays
-		timeArray = [];
-		docXArray = [];
-		docYArray = [];
-		gazeXArray = [];
-		gazeYArray = [];
-		headXArray = [];
-		headYArray = [];
-		headZArray = [];
-		headYawArray = [];
-		headPitchArray = [];
-		headRollArray = [];
-		//Counter for the index of arrays
-		indexCounter = -1;
-	
-		function PlotGaze(GazeData) {
-			//Update counter
-			indexCounter += 1;
-			
-			/*
-			GazeData.state // 0: valid gaze data; -1 : face tracking lost, 1 : gaze uncalibrated
-			GazeData.docX // gaze x in document coordinates
-			GazeData.docY // gaze y in document cordinates
-			GazeData.time // timestamp
-			*/
 
-			document.getElementById("TimeData").innerHTML = "Time: " + GazeData.time;
-			document.getElementById("GazeData").innerHTML = "GazeX: " + GazeData.GazeX + " GazeY: " + GazeData.GazeY;
-			document.getElementById("HeadPhoseData").innerHTML = " HeadX: " + GazeData.HeadX + " HeadY: " + GazeData.HeadY + " HeadZ: " + GazeData.HeadZ;
-			document.getElementById("HeadRotData").innerHTML = " Yaw: " + GazeData.HeadYaw + " Pitch: " + GazeData.HeadPitch + " Roll: " + GazeData.HeadRoll;
+		<script type = "text/javascript" >
+
+		// Init GazeData Array
+		var GazeDataArray = [];
+		
+		// new AJAX object
+		var xhttp = new XMLHttpRequest();
+		
+		var userIdStr = "<?php echo $userIdStr ?>"; // get user ID string from PHP
+		
+		// takes the GazeData object, adds userData and converts to JSON
+		// then sends the JSON by AJAX HTTP POST method to saveToDB.php
+		// where it is appended to the MongoDB object for userIdStr
+		function sendToDB(data) {
+			// begin ajax request
+			var jsonData = JSON.stringify(
+				{
+					"userIdStr": userIdStr,
+					"GazeDataArray": data
+				}
+			);
+  
+			xhttp.onreadystatechange = function() {
+				if (this.readyState == 4 && this.status == 200) {
+					console.log(""+this.responseText); // just log the output to JS console
+				}
+			};
 			
-			//Push gaze data into array
-			//timeArray.push("timestamp => " + GazeData.time);
-			timeArray.push(GazeData.time);
-			docXArray.push(GazeData.docX);
-			docYArray.push(GazeData.docY);
-			gazeXArray.push(GazeData.GazeX);
-			gazeYArray.push(GazeData.GazeY);
-			headXArray.push(GazeData.HeadX);
-			headYArray.push(GazeData.HeadY);
-			headZArray.push(GazeData.HeadZ);
-			headYawArray.push(GazeData.HeadYaw);
-			headPitchArray.push(GazeData.HeadPitch);
-			headRollArray.push(GazeData.Roll);
+			// post request to the PHP page
+			xhttp.open("POST", "saveToDB.php", true);
+
+			// the data type in the POST data to JSON
+			xhttp.setRequestHeader("Content-type", "application/json");
+
+			// convert javascript object to a JSON string and submit with the POST request
+			xhttp.send(jsonData);
+		}
+
+		// pushes each GazeData point to an array
+		// if the array is >= 10 elements, copy that array and append it to the MongoDB
+		// then empty the GazeDataArray
+		function saveData(GazeData){
+			console.log('.'); // debug
+			GazeDataArray.push(GazeData);
+			if (GazeDataArray.length >= 10){
+				sendToDB(GazeDataArray.slice()); // send a copy of the current array to the DB
+				
+				// if JS supported concurrency, we could lose data points here!
+				// because it isn't, these commands should be 'atomic'
+				
+				GazeDataArray = []; // empty the array 
+			}
+		}
+
+		// this is called everytime a GazaData message is received from the GazeCloud server
+		function PlotGaze(GazeData) {
+			saveData(GazeData); // send each GazeData point to the MongoDB
 			
-			//console.log(docXArray);
-			
+			// update gaze data on the page (after calibration)			
 			var x = GazeData.docX;
 			var y = GazeData.docY;
 			
@@ -82,40 +99,13 @@
 			}
 		}
 		
-		//How will be run this function to submit data? Button to begin next question and submit data?
-		function ajax() {
-			// begin ajax request
-			var xhttp = new XMLHttpRequest();
-  
-			/*xhttp.onreadystatechange = function() {
-				if (this.readyState == 4 && this.status == 200) {
-					document.getElementById("demo").innerHTML =
-					this.responseText;
-				}
-			};*/
-			
-			for (i = indexCounter; i < timeStamp.length; i += 1) {
-				//do something with the data
-				
-				
-				// post request to the PHP page
-				xhttp.open("POST", "xxxx.php", true);
-  
-				// the data type in the POST data to JSON
-				xhttp.setRequestHeader("Content-type", "application/json");
-				xhttp.withCredentials = true; 
-
-				// convert javascript object to a JSON string and submit with the POST request
-				xhttp.send(JSON.stringify(data));
-			}		
-		}
-		
         //////set callbacks/////////
 		GazeCloudAPI.OnCalibrationComplete =function(){ console.log('gaze Calibration Complete')  }
 		GazeCloudAPI.OnCamDenied =  function(){ console.log('camera  access denied')  }
 		GazeCloudAPI.OnError =  function(msg){ console.log('err: ' + msg)  }
 		GazeCloudAPI.UseClickRecalibration = true;
 		GazeCloudAPI.OnResult = PlotGaze; 
+		
 	</script>
 	</head>
 	<body >
@@ -132,5 +122,6 @@
          </p>
       </div>
       <div id ="gaze" style ='position: absolute;display:none;width: 100px;height: 100px;border-radius: 50%;border: solid 2px  rgba(255, 255,255, .2);	box-shadow: 0 0 100px 3px rgba(125, 125,125, .5);	pointer-events: none;	z-index: 999999'></div>
+	  <div id ="ajax"></div>
    </body>
 </html>

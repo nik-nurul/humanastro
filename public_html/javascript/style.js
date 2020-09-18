354f538d699a4231e49f1a8bc7515d0f6d4a0a40
fafa2908 <62166721+fafa2908@users.noreply.github.com>
2020-09-18 22:53:20 +1000

    Add style.js for navigation colour change

---
+++ public_html/javascript/style.js
@@ -0,0 +1,18 @@
+/*This javascript file is for the website's style that requires Javascript functions */
+
+"use strict"; //indicate the codes need to be executed in strict mode
+
+
+/* Change the navigation bar background color if scrolled passed the background image*/
+var navbar = document.getElementById("navibarID");
+
+window.addEventListener('scroll', function (e) {
+    var navbar = document.getElementById('navibarID');
+    if (document.documentElement.scrollTop || document.body.scrollTop >100) {
+            navbar.classList.add('nav-colored');
+            navbar.classList.remove('nav-transparent');
+        } else {
+            navbar.classList.add('nav-transparent');
+            navbar.classList.remove('nav-colored');
+        }
+});

dafc1567dc676400d1af5c04217fb5a600b724c9
fafa2908 <62166721+fafa2908@users.noreply.github.com>
2020-09-23 21:57:34 +1000

    Update style.js

--- public_html/javascript/style.js
+++ public_html/javascript/style.js
@@ -6,13 +6,28 @@
 /* Change the navigation bar background color if scrolled passed the background image*/
 var navbar = document.getElementById("navibarID");
 
-window.addEventListener('scroll', function (e) {
+window.addEventListener('scroll', function (e) { //if the page is scrolled
     var navbar = document.getElementById('navibarID');
-    if (document.documentElement.scrollTop >550 || document.body.scrollTop >550) {
+
+    //check if the current page is index page or not
+    if (window.location.href == "https://humanastro.csproject.org/dev/"){
+         if (document.documentElement.scrollTop>550 || document.body.scrollTop >550) { //if scrolled passed this height
             navbar.classList.add('nav-colored');
             navbar.classList.remove('nav-transparent');
         } else {
             navbar.classList.add('nav-transparent');
             navbar.classList.remove('nav-colored');
         }
+
+    } else {
+        if (document.documentElement.scrollTop>10 || document.body.scrollTop >10) { //if scrolled passed this height
+            navbar.classList.add('nav-colored');
+            navbar.classList.remove('nav-transparent');
+            console.log(window.location.href);
+        } else {
+            navbar.classList.add('nav-transparent');
+            navbar.classList.remove('nav-colored');
+        }
+
+    }
 });

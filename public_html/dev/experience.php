89a36a2b732d75dc9dbdf4ebaf1dfe48bb3857e4
SyabilAsyraf <62140671+SyabilAsyraf@users.noreply.github.com>
2020-09-28 16:20:20 +1000

    Debug

--- public_html/dev/experience.php
+++ public_html/dev/experience.php
@@ -195,7 +195,7 @@ require_once 'includes/feeback-link.php';
 				);
 				$result = $manager->executeBulkWrite($dbName.'.'.$Ucoll, $bulk);
 				
-				/*<script type="text/JavaScript">
+				<script>
 					document.getElementsByClassName("paragraph_font").style.visibility = "hidden";
 					document.getElementsByClassName("bttnsubmit").style.visibility = "hidden";
 				</script>*/
@@ -223,8 +223,8 @@ require_once 'includes/feeback-link.php';
 	} else {
 		// consent was false
 		echo '
-		<p>Consent was not given</p>
-		<p>Do something!</p>
+		<p class="paragraph_font">Thank you for your interest to participate in this study. Howevever, you 
+		need to be over 18 to proceed.</p>
 ';
 	}
 ?>
